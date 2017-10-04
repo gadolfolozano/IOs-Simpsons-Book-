8ad6ffd57076270bedc6bdcbb2cf069b00823785
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2017-10-03 22:34:47 -0500

    add basic flow

---
+++ Simpsons Book/Simpsons Book/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  Simpsons Book
+//
+//  Created by Adolfo Lozano Mendez on 3/10/17.
+//  Copyright © 2017 Adolfo Lozano Mendez. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var mImageView: UIImageView!
+    @IBOutlet weak var mTitleLabel: UILabel!
+    @IBOutlet weak var mDetailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
