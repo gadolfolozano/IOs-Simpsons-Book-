cd21aea5baa2007f19b73688673b91828a23877c
Adolfo Lozano Mendez <gadolfolozano@gmail.com>
2017-10-15 11:18:35 -0500

    setting basic list

---
+++ Simpsons Book/Simpsons Book/Simpson.swift
@@ -0,0 +1,17 @@
+//
+//  Simpson.swift
+//  Simpsons Book
+//
+//  Created by Adolfo Lozano Mendez on 4/10/17.
+//  Copyright © 2017 Adolfo Lozano Mendez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name = ""
+    var ocupation = ""
+    var image = UIImage()
+}
--- Simpsons Book/Simpsons Book/ViewController.swift
+++ Simpsons Book/Simpsons Book/ViewController.swift
@@ -8,20 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var mTableView: UITableView!
+    
+    var mSimpsonList = [Simpson]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mTableView.delegate = self
+        mTableView.dataSource = self
+        
+        let homer = Simpson()
+        homer.name = "Homero Simpson"
+        homer.ocupation = "Inspector de Seguridad"
+        print("image seting")
+        if let image = UIImage(named: "homersimpson.png") {
+            homer.image = image
+            print("image set")
+        }else {
+            print("image no set")
+        }
+        
+        let marge = Simpson()
+        marge.name = "Marge Simpson"
+        marge.ocupation = "Ama de Casa"
+        marge.image = UIImage(named: "marge.jpg")!
+        
+        let burns = Simpson()
+        burns.name = "Montgomery (Monty) Burns"
+        burns.ocupation = "Dueño de la planta nuclear de Spingfield"
+        burns.image = UIImage(named: "burns.jpg")!
+        
+        mSimpsonList.append(homer)
+        mSimpsonList.append(marge)
+        mSimpsonList.append(burns)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mSimpsonList.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = mSimpsonList[indexPath.row].name
+        return cell
+    }
 
 }
 
